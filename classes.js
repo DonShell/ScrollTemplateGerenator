5e82a89063ae7ab9d9b949217660c5fae41e02d9
DonShell <assum.dev@protonmail.com>
2022-07-15 01:04:48 -0300

    navegação de paginas funcionando, construindo carrossel

--- classes.js
+++ classes.js
@@ -10,6 +10,14 @@ class Coordinate
 		this.y = y;
 	}
 }
+//retirar essa funcao no final do projeto
+function sleep(millis)
+{
+    var date = new Date();
+    var curDate = null;
+    do { curDate = new Date(); }
+    while(curDate-date < millis);
+}
 
 class ScrollTemplate
 {
@@ -60,8 +68,8 @@ class ScrollTemplate
 	{
 		for(let i = 1; i <= this.numberOfPages ; i++)
 		{	
-			$(this.bodyId).append("<div id='screen"+i+"'></div>");
-			let page = this.bodyId + " #screen"+ i;
+			$(this.bodyId).append("<div id='page"+i+"'></div>");
+			let page = this.bodyId + " #page"+ i;
 			this.setDefaltPageLayout(page, (this.displaySize.y) * (i-1) , this.colorsBackgroundPages[i-1]);
 		}
 	}
@@ -84,5 +92,103 @@ class ScrollTemplate
 	show()
 	{
 		$(this.bodyId).show();
-	};
+		$("#load").fadeOut();
+	}
+
+
+
+	goToPage(page)
+	{
+		$("html , body").animate({scrollTop: this.displaySize.y * (page-1)});;
+	}
+
+
+
+	generateButtonPages()
+	{
+
+		for (let i = 1; i <= this.numberOfPages; i++) {
+			let index = "#page" + i;
+
+			$(index).append("<div class=navegationButtonConteiner></div>");
+
+			index += " .navegationButtonConteiner";
+			if(i != 1)
+			{
+				$(index).append("<div class='backButton buttonNavegation' onclick='template.goToPage("+ (i-1) +")' style='--aspect-ratio:1/1'></div>");
+			}
+
+			if(i != this.numberOfPages)
+			{
+				$(index).append("<div class='nextButton buttonNavegation' onclick='template.goToPage("+ (i+1) +")' style='--aspect-ratio:1/1'></div>");
+			}
+		}
+	}
+}
+class Carrossel
+{
+	numberOfPages;
+	fatherId;
+	carrosselId = "carrossel";
+
+	constructor(fatherId,size,numberOfPages)
+	{
+		this.fatherId = fatherId;
+		this.numberOfPages = numberOfPages;
+		this.size = size;
+		$(fatherId).append("<div id=" + this.carrosselId + "></div>")
+
+		this.carrosselId = fatherId + " #carrossel";
+
+		$(this.carrosselId).css({
+			'height':this.size.y + this.size.yType,
+			'width':this.size.x + this.size.xType,
+			'background-color':'#dfdfdf'
+		});
+		for (var i = 1; i <= numberOfPages; i++) {
+
+			$(this.carrosselId).append("<div id=pageC"+i+" class='pagesCarrossel' style='display:none'></div>")
+			
+		}
+		$(this.carrosselId + " #pageC1").show();
+		this.generateButtonPages();
+	
+	}
+
+	generateButtonPages()
+	{
+		$(this.carrosselId).append("<div id=nextButton class='buttonNavegationCarrossel'></div>");
+		$(this.carrosselId).append("<div id=backButton class='buttonNavegationCarrossel'></div>");
+	}
+
+}
+class CoordinateHTML
+{	
+	//use ("10%","50px")
+	constructor(x,y)
+	{
+		if(x.substring(x.length-2) == "px")
+		{	
+			this.x = parseInt(x.substring(0,x.length-2));
+			this.xType = "px";
+		} 
+		else if (x.substring(x.length-1) == "%")
+		{	
+			this.x = parseInt(x.substring(0,x.length-1));
+			this.xType = "%";
+		}
+
+
+		if(y.substring(y.length-2) == "px")
+		{	
+			this.y = parseInt(y.substring(0,y.length-2));
+			this.yType = "px";
+		} 
+		else if (y.substring(y.length-1) == "%")
+		{	
+			this.y = parseInt(y.substring(0,y.length-1));
+			this.yType = "%";
+		}	
+	}
+
 }
--- script.js
+++ script.js
@@ -4,6 +4,17 @@ function gerarEstrutura()
 {
 	template = new ScrollTemplate(["#a7badb","#262626","#bf9e9e","#4a2a2a"]);
 	// template.start();
+
+	$("#page1").append("<div id='title'> myClinic </div>")
+	template.generateButtonPages();
+
+	carrossel = new Carrossel("#page1",new CoordinateHTML("100%","60%"),3);
+
+
 }
+
+
+
+
 
 gerarEstrutura();
